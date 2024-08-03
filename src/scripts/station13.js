6c521ee29101540008bdb06c1e12a49edd63bad8
no215tyler <no.215.tyler@gmail.com>
2024-08-03 14:50:03 +0900

    station13: if文を使えるようになろう

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
-  // ここに背景色を変える処理を書く。
-
+  const boolFlg = document.getElementById("check");
+  const textElem = document.getElementById("text");
+  if (boolFlg.checked) {
+    textElem.setAttribute("style", "background-color: red;");
+  } else {
+    textElem.setAttribute("style", "background-color: transparent;");
+  }
 }
